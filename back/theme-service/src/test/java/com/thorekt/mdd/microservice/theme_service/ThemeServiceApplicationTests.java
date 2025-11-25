569eca3ea2a04fcb51f1134f2fc8e3d0b41da80d
rory <35566455+Thorekt@users.noreply.github.com>
2025-11-25 23:08:15 +0100

    feat(architecture-back): create initial microservices structure
    - Added base folder structure for the microservices architecture
    - Generated Spring Boot 4 applications for:
      - api-gateway (Reactive Web + Gateway + Security + Eureka client + Config client)
      - discovery-service (Eureka Server)
      - config-server (Spring Cloud Config Server)
      - user-service (Web + JPA + Security + MySQL + Eureka client)
      - theme-service (Web + JPA + Security + MySQL + Eureka client)
      - article-service (Web + JPA + Security + MySQL + Eureka client)
    - Added initial application.yml placeholders to each service (ports and spring.application.name to be configured next)
    - Architecture now ready for further configuration

---
+++ back/theme-service/src/test/java/com/thorekt/mdd/microservice/theme_service/ThemeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thorekt.mdd.microservice.theme_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
