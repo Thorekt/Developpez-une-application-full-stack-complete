b64609164395e3eacbeb8b1e37d828b8f8e40ea8
rory <35566455+Thorekt@users.noreply.github.com>
2025-11-28 04:41:20 +0100

    feat(user-service): add user entity
    - creation de l'entité user
    - import de la dépendance de validation

---
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/models/User.java
@@ -0,0 +1,43 @@
+package com.thorekt.mdd.microservice.user_service.models;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email"),
+        @UniqueConstraint(columnNames = "username")
+})
+@Data
+@Accessors(chain = true)
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = { "id" })
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NonNull
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NonNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NonNull
+    private String password;
+}
