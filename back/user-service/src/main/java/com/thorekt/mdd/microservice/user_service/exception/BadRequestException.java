db21ea25660c2473d40c3ec5c419ad052768af2d
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-03 21:04:12 +0100

    feat: add exceptions
    - Ajout de l'exception pour les données non trouvées
    - Ajout de l'exception pour les mauvaises requetes

---
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.thorekt.mdd.microservice.user_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+}
