f2de5eb95b668d9f7b3c58e72855adb3171cf04d
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-03 20:08:33 +0100

    feat(user-service): add JWT configuration
    ajout de la configuration jwt dans Security config :
    - Clé RSA privé et public à configuré dans application.yml
    - - security.jwt.public-key
    - - security.jwt.private-key
    - ajout jwtencoder dans le fichier

--- back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/config/SecurityConfig.java
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/config/SecurityConfig.java
@@ -1,11 +1,32 @@
 package com.thorekt.mdd.microservice.user_service.config;
 
+import org.springframework.core.io.Resource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.converter.RsaKeyConverters;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${security.jwt.private-key}")
+    private Resource privateKeyResource;
+
+    @Value("${security.jwt.public-key}")
+    private Resource publicKeyResource;
 
     /**
      * Password encoder bean using BCrypt hashing algorithm.
@@ -16,4 +37,20 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**
+     * JWT encoder bean using RSA keys.
+     * 
+     * @return JwtEncoder
+     * @throws Exception
+     */
+    @Bean
+    public JwtEncoder jwtEncoder() throws Exception {
+        RSAPrivateKey privateKey = RsaKeyConverters.pkcs8().convert(privateKeyResource.getInputStream());
+        RSAPublicKey publicKey = RsaKeyConverters.x509().convert(publicKeyResource.getInputStream());
+
+        RSAKey rsaKey = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        return new NimbusJwtEncoder(new ImmutableJWKSet<>(new JWKSet(rsaKey)));
+    }
+
 }
