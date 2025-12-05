a391c22f39a7fcbac5c08bb1df169cfd38f78c95
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-05 08:26:27 +0100

    feat(user-service): add LoginRequest DTO

---
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/dto/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.thorekt.mdd.microservice.user_service.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * Request DTO for login
+ * 
+ * @param loginValue User login value (email or username)
+ * @param password   User password
+ * 
+ * @author thorekt
+ */
+public record LoginRequest(
+                @NotBlank String loginValue,
+                @NotBlank String password) {
+
+}
