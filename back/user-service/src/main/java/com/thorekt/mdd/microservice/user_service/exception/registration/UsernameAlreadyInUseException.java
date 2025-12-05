14aefa7048b7489f10e9f921e2510c2dcd3f4313
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-05 08:29:43 +0100

    feat(user-service): add Registerexception and rework exception

--- back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/exception/registration/UsernameAlreadyInUseException.java
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/exception/registration/UsernameAlreadyInUseException.java
@@ -4,5 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.CONFLICT)
-public class UsernameAlreadyInUseException extends RuntimeException {
+public class UsernameAlreadyInUseException extends RegistrationException {
+    public UsernameAlreadyInUseException() {
+        super("USERNAME_ALREADY_IN_USE");
+    }
 }
