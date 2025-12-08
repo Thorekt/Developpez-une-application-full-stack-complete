bd7744c782cb45ab647f118b731c8590af5ef300
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-08 03:16:56 +0100

    feat(user-service): tell jpa where to find db entity

--- back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/UserServiceApplication.java
+++ back/user-service/src/main/java/com/thorekt/mdd/microservice/user_service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.thorekt.mdd.microservice.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.thorekt.mdd.microservice.user_service.model")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
