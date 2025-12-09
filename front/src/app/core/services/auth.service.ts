7221eee24edca2984e5b8617c192ca6ac555a4c3
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-09 11:15:19 +0100

    feat(front): add typed success/error responses and update AuthService
    - Ajout des modèles typés success-response et error-response
    - Mise à jour du AuthService pour retourner AuthResponse | ErrorResponse
    - Typage strict des méthodes register(), login() et me()
    - Amélioration de la sécurité de typage et de la gestion des réponses API

--- front/src/app/core/services/auth.service.ts
+++ front/src/app/core/services/auth.service.ts
@@ -5,6 +5,7 @@ import { RegisterRequest } from '../models/requests/register-request.model';
 import { AuthResponse } from '../models/responses/auth-response.model';
 import { LoginRequest } from '../models/requests/login-request.model';
 import { UserResponse } from '../models/responses/user-response.model';
+import { ErrorResponse } from '../models/responses/error-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,12 @@ export class AuthService extends ApiService {
 
   private prefix = '/auth';
 
-  register(data: RegisterRequest): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.baseUrl}${this.prefix}/register`, data);
+  register(data: RegisterRequest): Observable<AuthResponse | ErrorResponse> {
+    return this.http.post<AuthResponse | ErrorResponse>(`${this.baseUrl}${this.prefix}/register`, data);
   }
 
-  login(data: LoginRequest): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.baseUrl}${this.prefix}/login`, data);
+  login(data: LoginRequest): Observable<AuthResponse | ErrorResponse> {
+    return this.http.post<AuthResponse | ErrorResponse>(`${this.baseUrl}${this.prefix}/login`, data);
   }
 
   me(): Observable<UserResponse> {
