60ac782709765e007af09ab9830b2f5973b7fb6e
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-09 08:49:09 +0100

    feat(article-service): add aarticle controler

---
+++ back/article-service/src/main/java/com/thorekt/mdd/microservice/article_service/controller/ArticleController.java
@@ -0,0 +1,110 @@
+package com.thorekt.mdd.microservice.article_service.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.netflix.spectator.impl.PatternExpr.Or;
+import com.thorekt.mdd.microservice.article_service.dto.ArticleDto;
+import com.thorekt.mdd.microservice.article_service.dto.request.CreateArticleRequest;
+import com.thorekt.mdd.microservice.article_service.dto.response.ApiResponse;
+import com.thorekt.mdd.microservice.article_service.dto.response.ArticleListResponse;
+import com.thorekt.mdd.microservice.article_service.dto.response.ErrorResponse;
+import com.thorekt.mdd.microservice.article_service.dto.response.SuccessResponse;
+import com.thorekt.mdd.microservice.article_service.exception.NotFoundException;
+import com.thorekt.mdd.microservice.article_service.mapper.ArticleMapper;
+import com.thorekt.mdd.microservice.article_service.model.Article;
+import com.thorekt.mdd.microservice.article_service.service.ArticleService;
+
+import lombok.RequiredArgsConstructor;
+import utils.OrderEnum;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/article")
+public class ArticleController {
+    private final ArticleService articleService;
+
+    private final ArticleMapper articleMapper;
+
+    /**
+     * Find all articles by themes UUIDs in order.
+     * 
+     * @param themeUuids List of UUIDs of the themes
+     * @param order      OrderEnum indicating ascending or descending order
+     * @return List of Article entities
+     */
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> findArticlesByThemesUuidsInOrder(
+            @RequestParam("theme_uuids") List<UUID> themeUuids,
+            @RequestParam(value = "order", defaultValue = "DESC") OrderEnum order) {
+        try {
+            List<Article> articles = articleService.findAllArticlesByThemesUuidsInOrder(themeUuids, order);
+            List<ArticleDto> articleDtos = articles.stream()
+                    .map(articleMapper::toDto)
+                    .toList();
+            ArticleListResponse response = new ArticleListResponse(articleDtos);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ErrorResponse("INTERNAL_SERVER_ERROR"));
+        }
+    }
+
+    /**
+     * Find an article by UUID.
+     * 
+     * @param uuid UUID of the article
+     * @return Article entity
+     */
+    @GetMapping("/{uuid}")
+    public ResponseEntity<ApiResponse> findArticleByUuid(@PathVariable("uuid") UUID uuid) {
+        try {
+            Article article = articleService.findArticleByUuid(uuid);
+            ArticleDto articleDto = articleMapper.toDto(article);
+            return ResponseEntity.ok(new ArticleListResponse(List.of(articleDto)));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(404).body(new ErrorResponse(e.getMessage()));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(new ErrorResponse("INVALID_FORMAT"));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ErrorResponse("INTERNAL_SERVER_ERROR"));
+        }
+    }
+
+    /**
+     * Create a new article.
+     * 
+     * @param userUuid             UUID of the user
+     * @param createArticleRequest Request body containing themeUuid, title, and
+     *                             content
+     * @return ResponseEntity indicating the result of the operation
+     */
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse> createArticle(
+            @RequestParam("user_uuid") String userUuid,
+            @RequestBody CreateArticleRequest createArticleRequest) {
+        try {
+            articleService.createArticle(
+                    userUuid,
+                    createArticleRequest.themeUuid(),
+                    createArticleRequest.title(),
+                    createArticleRequest.content());
+            return ResponseEntity.ok().body(new SuccessResponse("ARTICLE_CREATED"));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(404).body(new ErrorResponse(e.getMessage()));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(new ErrorResponse("INVALID_FORMAT"));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ErrorResponse("INTERNAL_SERVER_ERROR"));
+        }
+    }
+}
---
+++ back/article-service/src/test/java/com/thorekt/mdd/microservice/article_service/controller/ArticleControllerTest.java
@@ -0,0 +1,263 @@
+package com.thorekt.mdd.microservice.article_service.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import com.thorekt.mdd.microservice.article_service.dto.ArticleDto;
+import com.thorekt.mdd.microservice.article_service.dto.request.CreateArticleRequest;
+import com.thorekt.mdd.microservice.article_service.dto.response.ApiResponse;
+import com.thorekt.mdd.microservice.article_service.dto.response.ArticleListResponse;
+import com.thorekt.mdd.microservice.article_service.exception.NotFoundException;
+import com.thorekt.mdd.microservice.article_service.mapper.ArticleMapper;
+import com.thorekt.mdd.microservice.article_service.model.Article;
+import com.thorekt.mdd.microservice.article_service.service.ArticleService;
+
+import utils.OrderEnum;
+
+@ExtendWith(MockitoExtension.class)
+public class ArticleControllerTest {
+    @Mock
+    ArticleService mockArticleService;
+
+    @Mock
+    ArticleMapper mockArticleMapper;
+
+    ArticleController classUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        classUnderTest = new ArticleController(mockArticleService, mockArticleMapper);
+    }
+
+    @Test
+    public void findArticlesByThemesUuidsInOrder_ShouldReturnListOfArticles() {
+        // Given
+        UUID themeUuid1 = UUID.randomUUID();
+        UUID themeUuid2 = UUID.randomUUID();
+        UUID articleUuid1 = UUID.randomUUID();
+        UUID articleUuid2 = UUID.randomUUID();
+        UUID userUuid1 = UUID.randomUUID();
+        UUID userUuid2 = UUID.randomUUID();
+
+        Article article1 = Article.builder().uuid(articleUuid1).title("Article 1").content("content 1")
+                .userUuid(userUuid1).themeUuid(themeUuid1).build();
+        Article article2 = Article.builder().uuid(articleUuid2).title("Article 2").content("content 2")
+                .userUuid(userUuid2).themeUuid(themeUuid2).build();
+
+        ArticleDto articleDto1 = ArticleDto.builder().uuid(articleUuid1).title("Article 1").content("content 1")
+                .userUuid(userUuid1).themeUuid(themeUuid1).createdAt("2025-01-01").build();
+        ArticleDto articleDto2 = ArticleDto.builder().uuid(articleUuid2).title("Article 2").content("content 2")
+                .userUuid(userUuid2).themeUuid(themeUuid2).createdAt("2025-01-02").build();
+
+        List<UUID> themeUuids = List.of(themeUuid1, themeUuid2);
+
+        Mockito.when(mockArticleService.findAllArticlesByThemesUuidsInOrder(themeUuids, OrderEnum.DESC))
+                .thenReturn(List.of(article1, article2));
+        Mockito.when(mockArticleMapper.toDto(article1)).thenReturn(articleDto1);
+        Mockito.when(mockArticleMapper.toDto(article2)).thenReturn(articleDto2);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticlesByThemesUuidsInOrder(themeUuids,
+                OrderEnum.DESC);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        ArticleListResponse body = (ArticleListResponse) response.getBody();
+        assertTrue(body.articles().size() == 2);
+        assertEquals("Article 1", body.articles().get(0).getTitle());
+        assertEquals("Article 2", body.articles().get(1).getTitle());
+        Mockito.verify(mockArticleService).findAllArticlesByThemesUuidsInOrder(themeUuids, OrderEnum.DESC);
+        Mockito.verify(mockArticleMapper).toDto(article1);
+        Mockito.verify(mockArticleMapper).toDto(article2);
+    }
+
+    @Test
+    public void findArticlesByThemesUuidsInOrder_ShouldHandleException() {
+        // Given
+        List<UUID> themeUuids = List.of(UUID.randomUUID(), UUID.randomUUID());
+
+        Mockito.when(mockArticleService.findAllArticlesByThemesUuidsInOrder(themeUuids, OrderEnum.DESC))
+                .thenThrow(new RuntimeException("Database error"));
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticlesByThemesUuidsInOrder(themeUuids,
+                OrderEnum.DESC);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(500), response.getStatusCode());
+        Mockito.verify(mockArticleService).findAllArticlesByThemesUuidsInOrder(themeUuids, OrderEnum.DESC);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void createArticle_ShouldCreateArticleAndReturnSuccessResponse() {
+        // Given
+        String userUuid = UUID.randomUUID().toString();
+        String themeUuid = UUID.randomUUID().toString();
+        String title = "New Article";
+        String content = "Article content";
+        CreateArticleRequest request = new CreateArticleRequest(themeUuid, title, content);
+
+        Mockito.doNothing().when(mockArticleService).createArticle(userUuid, themeUuid, title, content);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.createArticle(userUuid, request);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        Mockito.verify(mockArticleService).createArticle(userUuid, themeUuid, title, content);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void createArticle_ShouldHandleNotFoundException() {
+        // Given
+        String userUuid = UUID.randomUUID().toString();
+        String themeUuid = UUID.randomUUID().toString();
+        String title = "New Article";
+        String content = "Article content";
+        CreateArticleRequest request = new CreateArticleRequest(themeUuid, title, content);
+
+        Mockito.doThrow(new NotFoundException()).when(mockArticleService).createArticle(userUuid,
+                themeUuid, title, content);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.createArticle(userUuid, request);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
+        Mockito.verify(mockArticleService).createArticle(userUuid, themeUuid, title, content);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void createArticle_ShouldHandleIllegalArgumentException() {
+        // Given
+        String userUuid = "invalid-uuid";
+        String themeUuid = UUID.randomUUID().toString();
+        String title = "New Article";
+        String content = "Article content";
+        CreateArticleRequest request = new CreateArticleRequest(themeUuid, title, content);
+
+        Mockito.doThrow(new IllegalArgumentException("Invalid UUID")).when(mockArticleService).createArticle(userUuid,
+                themeUuid, title, content);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.createArticle(userUuid, request);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
+        Mockito.verify(mockArticleService).createArticle(userUuid, themeUuid, title, content);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void createArticle_ShouldHandleGenericException() {
+        // Given
+        String userUuid = UUID.randomUUID().toString();
+        String themeUuid = UUID.randomUUID().toString();
+        String title = "New Article";
+        String content = "Article content";
+        CreateArticleRequest request = new CreateArticleRequest(themeUuid, title, content);
+
+        Mockito.doThrow(new RuntimeException("Database error")).when(mockArticleService).createArticle(userUuid,
+                themeUuid, title, content);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.createArticle(userUuid, request);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(500), response.getStatusCode());
+        Mockito.verify(mockArticleService).createArticle(userUuid, themeUuid, title, content);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void findArticleByUuid_ShouldReturnArticle() {
+        // Given
+        UUID articleUuid = UUID.randomUUID();
+        UUID themeUuid = UUID.randomUUID();
+        UUID userUuid = UUID.randomUUID();
+        Article article = Article.builder().uuid(articleUuid).title("Article 1").content("content 1")
+                .userUuid(userUuid).themeUuid(themeUuid).build();
+        ArticleDto articleDto = ArticleDto.builder().uuid(articleUuid).title("Article 1").content("content 1")
+                .userUuid(userUuid).themeUuid(themeUuid).createdAt("2025-01-01").build();
+
+        Mockito.when(mockArticleService.findArticleByUuid(articleUuid)).thenReturn(article);
+        Mockito.when(mockArticleMapper.toDto(article)).thenReturn(articleDto);
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticleByUuid(articleUuid);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        ArticleListResponse body = (ArticleListResponse) response.getBody();
+        assertEquals("Article 1", body.articles().get(0).getTitle());
+        Mockito.verify(mockArticleService).findArticleByUuid(articleUuid);
+        Mockito.verify(mockArticleMapper).toDto(article);
+    }
+
+    @Test
+    public void findArticleByUuid_ShouldHandleNotFoundException() {
+        // Given
+        UUID articleUuid = UUID.randomUUID();
+
+        Mockito.when(mockArticleService.findArticleByUuid(articleUuid))
+                .thenThrow(new NotFoundException());
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticleByUuid(articleUuid);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
+        Mockito.verify(mockArticleService).findArticleByUuid(articleUuid);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void findArticleByUuid_ShouldHandleIllegalArgumentException() {
+        // Given
+        UUID articleUuid = UUID.randomUUID();
+
+        Mockito.when(mockArticleService.findArticleByUuid(articleUuid))
+                .thenThrow(new IllegalArgumentException("Invalid UUID"));
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticleByUuid(articleUuid);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
+        Mockito.verify(mockArticleService).findArticleByUuid(articleUuid);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+    @Test
+    public void findArticleByUuid_ShouldHandleGenericException() {
+        // Given
+        UUID articleUuid = UUID.randomUUID();
+
+        Mockito.when(mockArticleService.findArticleByUuid(articleUuid))
+                .thenThrow(new RuntimeException("Database error"));
+
+        // When
+        ResponseEntity<ApiResponse> response = classUnderTest.findArticleByUuid(articleUuid);
+
+        // Then
+        assertEquals(HttpStatusCode.valueOf(500), response.getStatusCode());
+        Mockito.verify(mockArticleService).findArticleByUuid(articleUuid);
+        Mockito.verifyNoInteractions(mockArticleMapper);
+    }
+
+}
