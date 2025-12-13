ee318cb331cc86686b7e9e40244ee76241304801
rory <35566455+Thorekt@users.noreply.github.com>
2025-12-13 07:41:38 +0100

    feat(front): for user logout when 401  and redirect him to home

--- front/src/app/core/interceptors/auth.interceptor.ts
+++ front/src/app/core/interceptors/auth.interceptor.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services/user/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -26,6 +30,15 @@ export class AuthInterceptor implements HttpInterceptor {
       }
     });
 
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.authService.logout();
+          this.router.navigate(['/?expired=true']);
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 }
